26a1a0e425865a06fd30b1395e66106340044ca9
Russ <lloydllames@gmail.com>
2024-05-20 00:36:49 +0800

    Update conn and add db backup

--- conn.php
+++ conn.php
@@ -1,13 +1,22 @@
 <?php
-    $servername = $_SERVER['SERVER_NAME'];
-    $username = $_SERVER['DATABASE_USERNAME'];
-    $password = $_SERVER['DATABASE_PASSWORD'];
-    $db_name = $_SERVER['DATABASE_NAME'];
+    $servername = "localhost";
+    $username = "root"; 
+    $password = "";
+    $db_name = "ducklab";
     $conn = new mysqli($servername,$username,$password,$db_name);
 
     if($conn->connect_error){
         die("Connection failed".$conn->connect_error);
     }
+    // $servername = $_SERVER['localhost'];
+    // $username = $_SERVER['root'];
+    // $password = $_SERVER[''];
+    // $db_name = $_SERVER['ducklab'];
+    // $conn = new mysqli($servername,$username,$password,$db_name);
+
+    // if($conn->connect_error){
+    //     die("Connection failed".$conn->connect_error);
+    // }
 
     $GLOBALS['DB_CON'] = $conn;
 ?>
